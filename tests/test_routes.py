d816f6a4afdf494d9843888ec5c0b8a52b31670f
dobbyzx <zifan.james.xiao@gmail.com>
2022-11-30 01:36:00 -0500

    added list endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,4 +123,20 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+    def test_list_accounts(self):
+        """It should return a list of all accounts"""
+        self._create_accounts(10)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 10)
+    
+    def test_list_accounts_empty(self):
+        """It should return an empty list if there are no accounts"""
+        self._create_accounts(0)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data, [])
+
+
