987f1493274546c26ad2b382861227f54c9ff6cd
dobbyzx <zifan.james.xiao@gmail.com>
2022-11-30 03:56:21 -0500

    added delete endpoint and added errorhandler test case

--- service/routes.py
+++ service/routes.py
@@ -115,8 +115,18 @@ def update_account(id):
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
-
+@app.route("/accounts/<id>", methods=["DELETE"])
+def delete_account(id):
+    """
+    Deletes a single account
+    This endpoint will delete an account based on the provided id
+    """
+    app.logger.info("Request to update an Account with id %s", id)
+    account = Account.find(id)
+    if account:
+        account.delete()
+    return "", status.HTTP_204_NO_CONTENT
+    
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
